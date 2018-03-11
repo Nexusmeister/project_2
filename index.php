f804fc20283f7aff6fdbfdfcb02e4b992d7c5358
Nexusmeister <37147173+Nexusmeister@users.noreply.github.com>
2018-03-11 20:07:12 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Project 2</title>
+</head>
+<body>
+	<div id="header">
+		<br>
+		<h1 id="v2">Projekt Stundenaufzeichnung </h1>
+		</div>
+ 
+ 	<div id="main">
+		<iframe src="main.php"></iframe>
+	</div>
+	<div id="footer">
+		
+		Copyright by Robin Kaltenbach
+	 	<a href="impressum.html">Impressum</a> 
+	</div>
+
+</body>
+</html>
